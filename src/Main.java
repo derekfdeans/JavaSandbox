37274aabe127c218b26ab0e03c10783fa85a8577
Derek <derekfdeans@icloud.com>
2023-01-13 19:10:26 -0500

    Console class from input stream

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.io.Console;
+import java.io.PrintWriter;
+
 public class Main {
     public static void main(String[] args) {
-        Returner<Integer> returner = new Returner<>(2353);
-        returner.compareValues(2323);
+        Console console = System.console();
+        if (console == null) {
+            System.err.println("no console.");
+            System.exit(1);
+        }
+
+        try (PrintWriter writer = console.writer()) {
+            writer.println("heyy");
+        }
+
+        char [] password = console.readPassword("what's your passowrd?");
+        for (char c : password) {
+            console.format(String.valueOf(c));
+        }
     }
 }
-
